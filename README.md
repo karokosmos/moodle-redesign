1f9c6f9928ead56c67d75fe81eeb8ceaf1777980
Karo Kosmos <okava035@edu.xamk.fi>
2019-09-09 17:28:19 +0300

    Add minor improvements

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Moodle redesign
+
+In this toy project I designed a new course page for Moodle (online studying platform). The main goal of this project was to get more familiar with CSS Grid and other modern CSS features.
--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ const nav = document.querySelector('.nav');
 const toggleBtn = nav.querySelector('.nav__toggle-btn');
 const toggleIcon = nav.querySelector('.nav__toggle-icon');
 
-// Toggle show/hide for the side navigation
-toggleBtn.addEventListener('click', _ => {
+// Toggle side navigation
+toggleBtn.addEventListener('click', e => {
   nav.classList.toggle('nav-is-hidden');
   toggleIcon.classList.toggle('rotate-icon');
 });
